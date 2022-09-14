a26d065fec1fe3d9181713887858d20f6ecab371
David Nilsson <davidn5013@yahoo.se>
2022-09-14 23:15:20 +0200

    David Nilsson packagge for any function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/davidn5013/davidstoolbox
+
+go 1.18
---
+++ hello.go
@@ -0,0 +1,8 @@
+package davidstoolbox
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello from davids-toolbox")
+	fmt.Println("This hello was last edit 20220914_224928_72")
+}
