a6c8022093b8f1c93c9394b5e5d9810dc40afd7b
David Nilsson <davidn5013@yahoo.se>
2022-09-19 18:18:21 +0200

    sumofPrimesUntil solves projecteuler 010

--- math.go
+++ math.go
@@ -1,3 +1,18 @@
 // Golang package with func for any use
 // math part
 package snickerboa
+
+import (
+	"github.com/fxtlabs/primes"
+)
+
+// sumofPrimesUntil sums all primenumber upp to n
+// Example 10 = 17
+func sumofPrimesUntil(n int) (sumPri uint64) {
+	p := primes.Sieve(n)
+	for i := 0; i < len(p); i++ {
+		sumPri = sumPri + uint64(p[i])
+	}
+	// fmt.Printf("%v\n", p)
+	return sumPri
+}
