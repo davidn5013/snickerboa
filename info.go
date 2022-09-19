5ae020b56372b411c9afc35fcf77c8121e1256ef
David Nilsson <davidn5013@yahoo.se>
2022-09-20 01:58:05 +0200

    Update of LastEdit()

--- info.go
+++ info.go
@@ -7,7 +7,7 @@ import "fmt"
 // LastEdit show last time snickerboa was change
 // Manual change for now
 func LastEdit() {
-	fmt.Println("This hello was last edit 20220919 18:38:39")
+	fmt.Println("This hello was last edit 20220920 01:57:20")
 }
 
 // Hello print "Hello from snickerboa"
