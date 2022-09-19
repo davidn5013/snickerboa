74c3b2b2fe2c62ae633f1d2508795b6ff702177a
David Nilsson <davidn5013@yahoo.se>
2022-09-19 20:02:23 +0200

    CheckPalindrome() added

--- math.go
+++ math.go
@@ -16,3 +16,22 @@ func SumofPrimesUntil(n int) (sumPri uint64) {
 	// fmt.Printf("%v\n", p)
 	return sumPri
 }
+
+// checkPalindrome check if a string can be read back to front
+// thanks for the steal (https://www.eventslooped.com/posts/interview-question-in-go-2/)
+func CheckPalindrome(testString string) bool {
+	isPalindrome := true
+	sLength := len(testString)
+
+	for i := 0; i < sLength/2; i++ {
+		currCharFwd := testString[i]           // character to compare going forward
+		currCharBwd := testString[sLength-1-i] // character to compare going from the end
+
+		if currCharFwd != currCharBwd {
+			isPalindrome = false // we found a non-match, it's not a palindrome
+			break                // no need to compare further
+		}
+	}
+
+	return isPalindrome
+}
